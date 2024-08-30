c83fd02e68667e96b836d0dcb2fc1fea046627f4
erfan.pirikhah <erfan.pirikhah1@gmail.com>
2024-08-30 21:13:15 +0330

    chore : add interceptor upload file

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -1,15 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+    UseInterceptors,
+    UploadedFile,
+    ParseFilePipe,
+    MaxFileSizeValidator,
+} from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { ApiConsumes } from '@nestjs/swagger';
+import { uploadFileS3 } from 'src/common/interceptors/upload-file.interceptor';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoriesService.create(createCategoryDto);
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(uploadFileS3('image'))
+  create(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [new MaxFileSizeValidator({ maxSize: 10 * 1024 * 1024 })],
+      }),
+    )
+    image: Express.Multer.File,
+    @Body()
+    createCategoryDto: CreateCategoryDto,
+  ) {
+    return {
+      image,
+      createCategoryDto,
+    };
+    // return this.categoriesService.create(createCategoryDto);
   }
 
   @Get()
@@ -23,7 +52,10 @@ export class CategoriesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
     return this.categoriesService.update(+id, updateCategoryDto);
   }
 
